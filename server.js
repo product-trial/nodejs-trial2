f49fe6c4cb08dab21bef5cfbd24cf506c00cb8b3
Shayne Burgess <shayneburgess@github.com>
2022-05-04 16:42:49 +0000

    Adds login screen and clean up

--- server.js
+++ server.js
@@ -1,29 +1,68 @@
 const express = require('express');
 const fs = require('fs');
 const path = require("path");
-const USER_ID = process.env.USER_ID;
+const bodyParser = require('body-parser');
 
+//get username and password from environment
+const PASSWORD = process.env.FACTS_PASSWORD;
+const USER_ID = process.env.GITHUB_USER;
+var loggedIn = false
+
+//read facts data file and parse
 const allFileContents = fs.readFileSync(path.join(__dirname, "data", "facts.txt"), 'utf-8');
 var lines = allFileContents.split('\n')
 
-console.log("Serving " + lines.length + " very fun facts.")
-
+//setup express
 var app = express();
-
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use("/public", express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
 
+//main page
 app.get('/', function (req, res) {
-  var linenumber = Math.floor(Math.random()*lines.length)
-  var line = lines[linenumber]
-  console.log("Showing fun fact " + linenumber+ ": " + line)
-  res.render("index", {userid: USER_ID, fact: line});
+  //simple one session login
+  if(!loggedIn)
+  {
+    return res.redirect('/Login');
+  }
+
+  //get a random fact and return page
+  var fact = getFunFact()
+  console.log("Showing fun fact: " + fact)
+  res.render("index", {fact: fact});
 })
 
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/static/login.html');
+});
+
+app.post('/login', (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  console.log("Received login request for: " + username)
+
+  //check username and password and log user in
+  if(username != USER_ID || password != PASSWORD || password == "")
+  {
+    loggedIn = false;
+    res.send("Login Failed. Invalid username or password.")
+  }else{
+    loggedIn = true;
+    return res.redirect('/');
+  }
+});
+
+function getFunFact()
+{
+  var linenumber = Math.floor(Math.random()*lines.length)
+  return lines[linenumber]
+}
+
+//Start server and listen on 8081
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Fun facts listening at http://%s:%s", host, port)
 })
